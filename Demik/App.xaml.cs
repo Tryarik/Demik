2317ab89bed56f4e0bb37d5d0028fa92eab3c887
IorikBog <IorikBog@DESKTOP-5DI6LIV>
2021-12-25 08:48:19 +0500

    Добавьте файлы проекта.

---
+++ Demik/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Demik
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities.DemikEntities Context
+        { get; } = new Entities.DemikEntities();
+
+        public static Entities.User CurrentUser = null;
+    }
+}
